d1ab9f28be76b5b7e1cd9a571a2723180280e8d1
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-05 09:57:28 +0530

    Basics Fonts & Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,28 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#3454d1',
+        secondary: '#fcfdaf',
+        terinary: '#00171F',
+        darkGrey: '#232325',
+        background: '#f2f2f2',
+        lightGrey: '#D3D3D3',
+        red: '#ff5943',
+        blue: '#35a7ff',
+        green: '#17b890',
+        white: '#ffffff',
       },
     },
   },
