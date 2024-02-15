aa1b62ceb9fed1b39d22ebe13a16bd51d103a652
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-15 12:59:18 +0530

    Colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="min-h-screen bg-background flex items-center text-center justify-center flex-col">
-      <div className="bg-cool shadow-xl rounded-xl p-12 sm:p-12 xl:p-24 relative">
-        <h1 className="text-background text-sm">we're working on it!</h1>
+      <div className="bg-orange shadow-xl rounded-xl p-12 sm:p-12 xl:p-24 relative">
+        <h1 className="text-background text-sm pb-4">we're working on it!</h1>
         <a
           href="mailto:contact@nazrulislam.dev"
           className="text-background text-xs m-8 link-hover1 relative"
