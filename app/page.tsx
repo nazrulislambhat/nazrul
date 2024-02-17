92672ad881619803aa05a05571ad042487527bd6
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-17 12:31:06 +0530

    some color

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Services from '../components/services';
 export default function Home() {
   return (
     <main className="min-h-screen bg-background flex items-center text-center justify-center flex-col">
-      <div className="bg-orange shadow-xl rounded-xl p-12 sm:p-12 xl:p-24 relative">
+      <div className="bg-terinary shadow-xl rounded-xl p-12 sm:p-12 xl:p-24 relative">
         <h1 className="text-background text-sm pb-4">we're working on it!</h1>
         <a
           href="mailto:contact@nazrulislam.dev"
