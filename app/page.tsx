c384f3e061fcfd043be0e818df445b8f5c5b364a
Nazrul Islam <nazrulislambhat@gmail.com>
2024-02-22 17:37:43 +0530

    Development

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
+import HeroCpt from '@/components/hero';
+import NavCpt from '../components/nav';
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background flex items-center text-center justify-center flex-col">
-      <div className="bg-terinary shadow-xl rounded-xl p-12 sm:p-12 xl:p-24 relative">
-        <h1 className="text-background text-sm pb-4">we're working on it!</h1>
-        <a
-          href="mailto:contact@nazrulislam.dev"
-          className="text-background text-xs m-8 link-hover1 relative"
-        >
-          get in touch
-        </a>
-      </div>
+    <main>
+      <NavCpt />
+      <HeroCpt />
     </main>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import Image from 'next/image';
+import Logo from '../assets/logo.png';
 export default function NavCpt() {
-  return <h1>Nav Component</h1>;
+  return (
+    <nav>
+      <Image src={Logo} alt="logo" width={64} height={64} />
+    </nav>
+  );
 }
