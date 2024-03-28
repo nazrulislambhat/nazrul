f1f8fbf289ac9b8cd75027f5f18c80a1ebdd756f
Nazrul Islam <nazrulislambhat@gmail.com>
2024-03-28 08:59:18 +0530

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="max-width">
+      <meta name="theme-color" content="#4831D3" />
       <body className={`${comfortaa.className}`}>
         <Providers>{children}</Providers>
       </body>
