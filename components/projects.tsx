375da8ac1bf66856241e0fd7f38985fa95d5c940
Nazrul Islam <nazrulislambhat@gmail.com>
2024-03-29 09:05:19 +0530

    Changes Resume

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
         </p>
         <span className="absolute inset-x-0 bottom-1 h-2 w-full bg-secondary z-0 opacity-80"></span>
       </div>
-      <div className="flex gap-1 opacity-80">
+      <div className="opacity-80">
         <p className="text-xs font-semibold">
           Over the years, I've engaged in numerous personal and professional
           projects, with some documented on my{' '}
@@ -19,13 +19,13 @@ export default function Projects() {
             Github
           </Link>{' '}
           profile and others showcased on my website at
+          <Link
+            href="/work"
+            className="hover:animate-text font-bold pb-1 text-xs hover:opacity-100 mx-1 text-primary link-primary hover:scale-105 w-fit opacity-100"
+          >
+            work
+          </Link>
         </p>
-        <Link
-          href="/work"
-          className="hover:animate-text font-bold pb-1 text-xs hover:opacity-100  text-primary link-primary hover:scale-105 w-fit opacity-100"
-        >
-          work
-        </Link>
       </div>
     </div>
   );
