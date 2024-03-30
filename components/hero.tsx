600c59799411f44ab306cb4fc7b6a7cdb530795e
Nazrul Islam <nazrulislambhat@gmail.com>
2024-03-30 08:54:27 +0530

    Changes Resume

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 
-export default function Hero() {
-  return (
-    <main className="flex flex-col h-[600px] text-third justify-center items-center text-center border-2">
-      <span className="name flex items-center justify-center gap-4 uppercase animate-text  bg-gradient-to-r from-secondary to-primary bg-clip-text text-transparent font-bold opacity-100 text-4xl">
-        01
-        <span className="seperator block rounded-full text-sm bg-third w-8 h-0.5"></span>
-        Nazrul Islam
-      </span>
-      <h1 className="intro">
-        Software Engineer - Full Stack Developer - Code Enthusiast
-      </h1>
-    </main>
-  );
+function Hero() {
+  return <div></div>;
 }
+
+export default Hero;
