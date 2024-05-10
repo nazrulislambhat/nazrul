2c25edd219a47642003cebef6c305a9b03631c22
Nazrul Islam <nazrulislambhat@gmail.com>
2024-05-10 19:13:32 +0530

    Changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,17 @@
 function Hero() {
   return (
-    <div className="relative text-white  my-16 px-64 pt-32 bg-third">
-      <div className=" h-[500px] relative">
-        <h1 className="heading text-7xl pb-24">
+    <div className="relative h-[100vh] xl:h-[500px] text-white  px-8 xl:mb-16 xl:px-64 pt-32 bg-primary">
+      <div className="  relative">
+        <h1 className="heading xl:text-7xl xl:pb-24">
           I am Nazrul Islam , and I engineer code that shapes destinies.
         </h1>
 
-        <p className="subheading-one py-8  uppercase max-w-lg float-right tracking-wide text-sm">
+        <p className="subheading-one py-8  uppercase max-w-lg xl:float-right tracking-wide text-sm">
           Nazrul Islam is a passionate software developer with a knack for
           turning lines of code into digital wonders.
         </p>
       </div>
-      <p className="about h-full max-w-7xl text-6xl  leading-normal pb-32">
+      <p className="about h-full xl:max-w-7xl xl:text-6xl text-xs  leading-normal xl:pb-32">
         Nazrul Islam, an enthusiastic software developer, excels in transforming
         lines of code into digital marvels. Armed with a computer science
         background and a talent for inventive problem-solving, Nazrul Islam is
