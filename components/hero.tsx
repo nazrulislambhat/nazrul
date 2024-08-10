cd35bba5a5fd64011945bc226fcf32aaa9006ee4
Nazrul Islam <58062-nazrulislambhat@users.noreply.drupalcode.org>
2024-08-10 12:22:49 +0530

    about page changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,8 @@
 function Hero() {
   return (
-    <div className="relative h-screen xl:min-h-[1440px] text-white px-8 xl:mb-16 xl:px-64 pt-32 bg-primary">
+    <div className="relative xl:min-h-[1440px] text-white px-8 xl:mb-16 xl:px-64 pt-32 pb-16 bg-primary">
       <div className="relative">
-        <h1 className="heading text-xl xl:text-5xl xl:pb-16 selection:bg-secondary selection:text-primary">
+        <h1 className="heading text-xl xl:text-5xl xl:pb-16 selection:bg-secondary selection:text-primary pb-12">
           Software developer and sometimes writer. My daily routine consists of
           (but not limited to) drinking coffee, coding, writing, overcoming
           boredom 😉.{' '}
@@ -11,12 +11,12 @@ function Hero() {
           </span>
         </h1>
 
-        <p className="subheading-one selection:bg-secondary selection:text-primary py-8 uppercase max-w-lg xl:float-right tracking-wide text-xs">
+        <p className="subheading-one hidden xl:block selection:bg-secondary selection:text-primary py-8 uppercase max-w-lg xl:float-right tracking-wide text-xs">
           Nazrul Islam is a passionate software developer with a knack for
           turning lines of code into digital wonders.
         </p>
       </div>
-      <p className="about h-screen selection:bg-secondary selection:text-primary opacity-50 xl:text-[3rem] xl:leading-[3rem] xl:pt-52 xl:mb-32">
+      <p className="about selection:bg-secondary selection:text-primary opacity-50 xl:text-[3rem] xl:leading-[3rem] xl:pt-52 xl:mb-32">
         Nazrul Islam, an enthusiastic software developer, excels in transforming
         lines of code into digital marvels. Armed with a computer science{' '}
         <span className="border-b-2 border-secondary">Masters Degree</span> and
