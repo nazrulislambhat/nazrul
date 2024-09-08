553f62f712b9527d32264f3b0aac84fd315f67d9
Nazrul Islam <nazrul.islam@ama-assn.org>
2024-09-08 10:03:51 +0530

    +

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <div className="relative xl:min-h-[1440px] text-white px-8 xl:mb-16 xl:px-64 pt-32 pb-16 boxed bg-primary">
+    <div className="relative xl:min-h-[40rem] text-white px-8 xl:mb-16 xl:px-64 pt-32 pb-16 boxed bg-primary">
       <div className="relative">
         <h1 className="heading text-xl xl:text-5xl xl:pb-16 selection:bg-secondary selection:text-primary pb-12">
           Software developer and sometimes writer. My daily routine consists of
@@ -11,12 +11,12 @@ function Hero() {
           </span>
         </h1>
 
-        <p className="subheading-one hidden xl:block selection:bg-secondary selection:text-primary py-8 uppercase max-w-lg xl:float-right tracking-wide text-xs">
+        <p className="subheading-one xl:block selection:bg-secondary selection:text-primary py-8 uppercase max-w-xs xl:max-w-lg clear-both xl:float-right tracking-wide text-xs xl:text-sm">
           Nazrul Islam is a passionate software developer with a knack for
           turning lines of code into digital wonders.
         </p>
       </div>
-      <p className="about selection:bg-secondary text-sm selection:text-primary xl:text-[3rem] xl:leading-[3rem] xl:pt-52 xl:mb-32">
+      {/* <p className="about selection:bg-secondary text-sm selection:text-primary xl:text-[3rem] xl:leading-[3rem] xl:pt-52 xl:mb-32">
         Nazrul Islam, an enthusiastic software developer, excels in transforming
         lines of code into digital marvels. Armed with a computer science{' '}
         <span className="border-b-2 border-secondary">Masters Degree</span> and
@@ -25,7 +25,7 @@ function Hero() {
         the beauty of nature, enjoying outdoor adventures. This multifaceted
         approach allows Nazrul Islam to find harmony not only in software
         development but also in life's diverse experiences.
-      </p>
+      </p> */}
     </div>
   );
 }
