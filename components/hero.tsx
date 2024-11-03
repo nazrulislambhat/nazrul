15fbc90f40f87358a638a10911122b4d2b2994ec
Nazrul Islam <nazrul.islam@ama-assn.org>
2024-11-03 12:57:30 +0530

    Some Resume Changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,20 +1,26 @@
+import Link from 'next/link';
 function Hero() {
   return (
-    <div className="relative xl:min-h-fit text-white px-8 xl:mb-16 xl:px-64 pt-32 pb-16 boxed bg-primary">
+    <div className="relative  text-white px-8 xl:mb-16 xl:px-64 pt-32 pb-16 boxed bg-primary">
       <div className="relative">
-        <h1 className="heading text-xl xl:text-5xl xl:pb-16 selection:bg-secondary selection:text-primary pb-12">
-          Software developer and sometimes writer. My daily routine consists of
+        <h1 className="heading text-xl xl:text-5xl pb-8 xl:pb-12 selection:bg-secondary selection:text-primary">
+          Software Engineer and sometimes writer. My daily routine consists of
           (but not limited to) drinking coffee, coding, writing, overcoming
-          boredom 😉.{' '}
-          <span className="border-b-2 border-secondary">
-            Living in Namma Bengaluru
-          </span>
+          boredom 🚀
         </h1>
-
-        {/* <p className="subheading-one xl:block selection:bg-secondary selection:text-primary py-8 uppercase max-w-xs xl:max-w-lg clear-both xl:float-right tracking-wide text-xs xl:text-sm">
-          Nazrul Islam is a passionate software developer with a knack for
-          turning lines of code into digital wonders.
-        </p> */}
+        <span className="border-b-1 border-secondary">
+          Living in Namma Bengaluru
+        </span>
+        <p className="subheading-one selection:bg-secondary selection:text-primary mt-6 mb-12 uppercase tracking-wide text-xs">
+          Your trusted partner for strategy, design, development.
+        </p>
+        <Link
+          href="https://cal.com/nazrul/"
+          target="_blank"
+          className="text-primary border-1 text-xs px-6 py-4 font-semibold border-secondary bg-secondary rounded-full hover:border-secondary hover:bg-transparent hover:text-secondary"
+        >
+          Schedule a call 📅
+        </Link>
       </div>
       {/* <p className="about selection:bg-secondary text-sm selection:text-primary xl:text-[3rem] xl:leading-[3rem] xl:pt-52 xl:mb-32">
         Nazrul Islam, an enthusiastic software developer, excels in transforming
