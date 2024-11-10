9ce3ae80fd4aef1be540b00e3cd8ca60b4a9f97f
Nazrul Islam <nazrul.islam@ama-assn.org>
2024-11-10 18:40:01 +0530

    +

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -7,14 +7,14 @@ import Header from '@/components/header';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
 function ComingSoon() {
-  const [progress, setProgress] = useState(20.0001);
+  const [progress, setProgress] = useState(17.319);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setProgress((prevProgress) =>
         prevProgress >= 100 ? 100 : prevProgress + 0.02
       );
-    }, 50); // Update every 100 milliseconds
+    }, 1000); // Update every 100 milliseconds
 
     return () => clearInterval(interval);
   }, []);
@@ -22,35 +22,46 @@ function ComingSoon() {
     <div className="">
       <Header />
       <div className="flex flex-col items-start justify-center align-middle boxed h-screen rounded bg-primary">
-        <h1 className="text-white text-3xl xl:text-8xl flex justify-center items-center align-middle mx-8 mb-4 ">
-          in the making
-        </h1>
-        <span
-          className="h-fit text-xs max-w-[85%] xl:max-w-[95%] font-bold px-4 py-1 bg-secondary rounded-xl mx-8 mb-4 bg-gradient-to-r text-primary from-secondary to-red"
+        <motion.h1
+          className="text-white text-3xl xl:text-8xl flex justify-center items-center align-middle mx-8 mb-4 selection:bg-secondary selection:text-primary "
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          in the making.....
+        </motion.h1>
+        <motion.span
+          className="h-fit text-lg xl:text-xl max-w-[85%] xl:max-w-[95%] font-bold px-4 py-1 bg-secondary rounded-md mx-8 mb-4 bg-gradient-to-r text-primary from-secondary via-newPrimary to-red "
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
           style={{
             width: `${progress}%`,
             transition: 'width 0.1s linear',
           }}
         >
-          {progress.toFixed(4)}%
-        </span>
-        <Link
-          className="text-secondary font-semibold text-xs mx-8 mb-4 link "
+          {progress.toFixed(3)}%
+        </motion.span>
+        <motion.a
+          className="text-secondary font-semibold text-xs mx-8 mb-4 link selection:bg-secondary selection:text-primary"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
           href="mailto:nazrul@nazrulislam.dev"
         >
           nazrul@nazrulislam.dev
-        </Link>
+        </motion.a>
         <motion.a
           href="https://cal.com/nazrul"
           target="_blank"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 2 }}
           rel="noopener noreferrer"
-          className="hover:link-primary hover:text-secondary mx-8 mb-4 hover:opacity-100 text-xs text-background link hover:scale-105 flex items-center w-fit opacity-80"
+          className="hover:link-primary hover:text-secondary mx-8 mb-4 selection:bg-secondary selection:text-primary hover:opacity-100 text-[8px] text-secondary link hover:scale-105 flex text-semibold  items-center w-fit "
         >
-          meeting
-          <FaExternalLinkAlt className="ml-1 text-background  hover:opacity-100 hover:text-secondary text-[10px] link flex items-center opacity-80 w-fit" />
+          schedule a meeting
+          <FaExternalLinkAlt className="ml-1 text-secondary  hover:opacity-100 hover:text-secondary text-[10px] link flex items-center w-fit" />
         </motion.a>
       </div>
     </div>
