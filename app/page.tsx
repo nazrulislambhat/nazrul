ae97394452a914847d0711cb26c68ab2a61ac213
Nazrul Islam <nazrulislambhat@gmail.com>
2025-02-09 23:18:38 +0530

    +

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ import ContactComponent from '@/components/contact';
 import FooterComponent from '@/components/footer';
 import ProjectsComponent from '@/components/projects';
 import ComingSoon from './coming-soon/page';
+import ComingSoonComponent from '@/components/something-cool';
 export default function Home() {
   return (
     <main className="bg-primary">
+      <ComingSoonComponent />
       <Header />
       <Hero />
       <AboutComponent />
---
+++ components/something-cool.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import { Mail, ExternalLink } from 'lucide-react';
+
+export default function ComingSoonComponent() {
+  return (
+    <div className="px-6 md:px-12 xl:px-16 bg-third 2xl:px-24 boxed1920 py-8">
+      <p className="text-secondary text-xs font-semibold w-fit">
+        🚧 Pardon our dust –--- greatness is in progress! 🚀
+      </p>
+      <p className="text-background text-xs my-4">
+        This site is under construction & bugs are welcomed at {' '}
+        <Link
+          href="mailto:n@nazrulislam.dev"
+          className="hover:border-b-1 border-secondary font-bold text-secondary hover:text-background hover:border-background"
+        >
+          nazrul@nazrulislam.dev
+        </Link>{' '}
+        🐞
+      </p>
+      <p className="text-background text-xs">
+        Progress is here:{' '}
+        <Link
+          href="https://github.com/nazrulislambhat/nazrul"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-secondary hover:border-b-1 font-bold text-secondary hover:text-background hover:border-background"
+        >
+          GitHub
+        </Link>
+        ⚡
+      </p>
+    </div>
+  );
+}
