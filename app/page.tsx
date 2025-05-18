c70e1415276b6b8568dcd9b9858541df7f9712bb
Nazrul Islam <nazrulislambhat@gmail.com>
2025-05-18 11:42:01 +0530

    +

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Contact from '@/components/Contact';
-import FooterLower from '@/components/FooterLower';
-import SomethingCoolSoon from '@/components/SomethingCoolSoon';
-import Years from '@/components/Years';
-import Services from '@/components/Services';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import FooterLower from '../components/FooterLower';
+import SomethingCoolSoon from '../components/SomethingCoolSoon';
+import Years from '../components/Years';
+import Services from '../components/Services';
 export default function Home() {
   return (
     <main className="boxed">
