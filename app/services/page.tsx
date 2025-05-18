5412fa1111f5ff9708dae7f7d0483f8644a47563
Nazrul Islam <nazrulislambhat@gmail.com>
2025-05-18 11:35:04 +0530

    +

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Partner from '@/components/PartnerComponent';
 import Nav from '@/components/Navbar';
 import Header from '@/components/Header';
 function Services() {
   return (
     <div className="bg-black">
       <Header />
-      <Partner />
     </div>
   );
 }
