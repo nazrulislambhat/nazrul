dda5536b11a47c5e4339ab5d75051c5bea0fc99c
Nazrul Islam <nazrulislambhat@gmail.com>
2025-05-18 12:03:29 +0530

    +

--- components/FooterLower.tsx
+++ components/FooterLower.tsx
@@ -3,31 +3,22 @@ import React from 'react';
 
 export default function FooterLower() {
   return (
-    <div className="relative bg-coolWhite py-20 px-6 md:px-12 xl:px-16 2xl:px-24">
-      <ul className="flex justify-center items-center gap-4">
+    <div className="relative bg-coolWhite py-6 px-6 md:px-12 xl:px-16 2xl:px-24">
+      <ul className="flex justify-center items-center">
         <li>
-          <Link href="/licences" className="link-primary text-xs font-semibold">
-            Licences
-          </Link>
-        </li>
-        |
-        <li>
-          <p className="text-xs font-semibold">
-            Developed with 🧑🏻‍💻 by Nazrul Islam
+          <p className="text-[10px] font-semibold">
+            Developed with 🧑🏻‍💻 by{' '}
+            <Link
+              href="https://nazrulislam.dev"
+              className="lin-primary text-primary"
+            >
+              Nazrul Islam
+            </Link>
           </p>
         </li>
-        |
+        <span className="mx-1">-</span>
         <li>
-          <Link
-            href="/changelog"
-            className="link-primary text-xs font-semibold"
-          >
-            Changelog
-          </Link>
-        </li>
-        |
-        <li>
-          <p className="text-xs font-semibold">Copyright © 2025</p>
+          <p className="text-[10px] font-semibold">Copyright © 2025</p>
         </li>
       </ul>
     </div>
